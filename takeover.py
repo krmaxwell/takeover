eecf94dce1fab2f2795ba274da8092135b6adaaf
Kyle Maxwell <krmaxwell@gmail.com>
2013-10-06 14:59:35 -0500

    Added PyGame dependency

--- takeover.py
+++ takeover.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
+import pygame
+import random
+
+from pygame.locals import *
 
 # define constants for objects on the board
 BOARD_BLANK = 0
@@ -6,11 +10,20 @@ BOARD_VILLAGE_1 = 1
 BOARD_VILLAGE_2 = 2
 BOARD_WARRIOR_1 = 3
 BOARD_WARRIOR_2 = 4
+# able to expand as necessary for obstacles, powerups, etc.
 
 # initialize board as global variable
 board = [[]]
 
-def init_board()
+# necessary for PyGame to get set up
+pygame.init()
+
+def roll_die():
+    # just for ease of reading later
+    return random.randrange(6)+1
+
+def init_board():
+    # default board but may eventually offer different types
     board = [[BOARD_BLANK for i in xrange(8)] for j in xrange(8)]
     board[0][0] = BOARD_VILLAGE_1
     board[0][1] = BOARD_WARRIOR_1
